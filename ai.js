e64c138310a0f091c9b2c8e3abd598ce37236b65
xondamir011 <xondamirmadaliyev79@gmail.com>
2026-05-01 17:57:22 +0500

    remove

--- ai.js
+++ ai.js
@@ -1,32 +1,34 @@
 import axios from "axios";
-import dotenv from "dotenv";
-import { model } from "mongoose";
 
-dotenv.config();
+export async function checkCode(code) {
+  try {
+    const res = await axios.post(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        model: "openai/gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a strict programming teacher. Check the code and give short feedback and score from 0 to 10."
+          },
+          {
+            role: "user",
+            content: code
+          }
+        ]
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
 
-export const askAI = async (question) => {
-    try{
-        const res = await axios.post("https://openrouter.ai/api/v1/chat/completions", {
-            model: "openai/gpt-3.5-turbo",
-            messages: [
-                {
-                    role: "system",
-                    content: "Kodga 0-10 ball ber va qisqa feedback yoz"
-                }, 
-                {
-                    role: "user",
-                    content: code
-                }
-            ]
-        }, 
-         {
-            headers: {
-                Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
-            }
-         }
-      );
-        return res.data.choices?.[0]?.message?.content || "Javob topilmadi 😅";
-    } catch (err) {
-        return "AI xato berdi 😅";
-    }
+    return res.data.choices?.[0]?.message?.content || "No feedback";
+  } catch (err) {
+    console.log("AI ERROR:", err.message);
+    return "❌ AI tekshira olmadi";
+  }
 }
--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const connectDB = async () => {
    try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("🟢 MongoDB ulandi");
-    
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,  
+      useUnifiedTopology: true
+   });
+
+   console.log("🟢 MongoDB ulandi");
+
    } catch (err) {
     console.log("🔴 MongoDB ulanmadi");
+    process.exit(1);
    }
 };
